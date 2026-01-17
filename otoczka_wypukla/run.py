9124b8a2f34664afd1dcb9138efcf4c38b6700bd
TymonPawelczyk <tymon.pawelczyk@gmail.com>
2026-01-17 14:31:56 +0100

    docs: Enhance main application script documentation and startup messages

--- otoczka_wypukla/run.py
+++ otoczka_wypukla/run.py
@@ -1,7 +1,18 @@
+"""
+Główny plik uruchamiający aplikację.
+
+Ten skrypt uruchamia serwer webowy z aplikacją do obliczania otoczki wypukłej.
+Po uruchomieniu, aplikacja będzie dostępna pod adresem: http://localhost:5000
+"""
+
 from web import create_app
 
+# Tworzymy aplikację (ładujemy wszystkie potrzebne komponenty)
 app = create_app()
 
+# Uruchamiamy aplikację tylko jeśli ten plik został uruchomiony bezpośrednio
 if __name__ == '__main__':
     print("Uruchamianie serwera Geometrii Obliczeniowej...")
+    print("Otwórz przeglądarkę i wejdź na: http://localhost:5000")
+    # Uruchamiamy serwer z trybem debugowania i na porcie 5000
     app.run(debug=True, port=5000)
