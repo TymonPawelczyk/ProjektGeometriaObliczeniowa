14a2fdbc6a307d6f9f535875f5126ede91a8cc0b
TymonPawelczyk <tymon.pawelczyk@gmail.com>
2026-01-17 14:31:52 +0100

    refactor: Update configuration file with detailed comments and structure

--- otoczka_wypukla/config.py
+++ otoczka_wypukla/config.py
@@ -1,9 +1,20 @@
+"""
+Plik konfiguracyjny aplikacji.
+
+Zawiera ustawienia dla różnych trybów działania aplikacji:
+- Tryb deweloperski (do pracy nad projektem) - z debugowaniem włączonym
+- Tryb produkcyjny (dla finalnej wersji) - z debugowaniem wyłączonym
+"""
+
 class Config:
-    DEBUG = True
-    TESTING = False
+    """Podstawowa konfiguracja - wspólne ustawienia dla wszystkich trybów."""
+    DEBUG = True        # Czy włączyć tryb debugowania (pokazywanie szczegółów błędów)
+    TESTING = False     # Czy aplikacja jest w trybie testowania
 
 class ProductionConfig(Config):
-    DEBUG = False
+    """Konfiguracja dla wersji produkcyjnej (użytkowej) aplikacji."""
+    DEBUG = False       # W wersji produkcyjnej wyłączamy debugowanie dla bezpieczeństwa
 
 class DevelopmentConfig(Config):
-    DEBUG = True
+    """Konfiguracja dla trybu deweloperskiego (podczas tworzenia aplikacji)."""
+    DEBUG = True        # Włączamy debugowanie, aby łatwiej znajdować błędy
